6eab349faf85687ee62b29694d0a59e553a111ab
sanderengel <saet@itu.dk>
2025-11-12 15:20:54 +0100

    further work on detecting trees

--- detect_tree.py
+++ detect_tree.py
@@ -7,7 +7,21 @@ def is_directed(G: Graph) -> bool:
             return False
     return True
 
+def has_cycle(G) -> bool:
+    # Keep three sets for the vertices
+    unvisited = set(G.V) # Initialize all vertices as unvisited
+    visiting = set()
+    visited = set()
+
+    # Iterate over all vertices u in V
+    pass
+
+
 
 def is_tree(G: Graph) -> bool:
-    return is_directed
+    # Return False if undirected
+    if not is_directed(G):
+        return False
+    
+
 
